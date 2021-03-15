e9b12bae95971a1b8bec9cb505702956af3e8bdb
bdpqchen <bdpqchen@gmail.com>
2021-03-15 23:28:34 +0800

    make runnable at tag v2.0-alpha

--- app/src/main/java/com/bdpqchen/learnglide/MainActivity.java
+++ app/src/main/java/com/bdpqchen/learnglide/MainActivity.java
@@ -2,6 +2,9 @@ package com.bdpqchen.learnglide;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    ImageView iv = findViewById(R.id.iv);
+    Glide.load("https://avatars.githubusercontent.com/u/22075571?s=460&u=40ec74aaba883289f5352b6d557287c66eba0253&v=4").into(iv);
   }
 }
